73f7bf5db153e581f6f15a3c962e84a5185267b0
Gabriel Barboza <gabrielmbarboza@gmail.com>
2019-05-05 20:16:13 -0300

    Adds validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates_uniqueness_of :cpf, on: :create, message: "must be unique"
+  validates_presence_of :cpf, on: :create, message: "can't be blank"
+  
+  has_many :orders
 end
