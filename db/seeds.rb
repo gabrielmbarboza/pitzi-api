0cc4922a561cdafc3db4f640f116d8a0292e6c23
Gabriel Barboza <gabrielmbarboza@gmail.com>
2019-05-12 23:25:05 -0300

    Creates seed to Users and Orders

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+  user = User.create(
+    name: Faker::Name.name, 
+    email: Faker::Internet.email, 
+    cpf: Faker::IDNumber.brazilian_citizen_number
+  )
+
+  Order.create(
+    device_imei: Faker::Number.number(15),
+    device_model: Faker::Device.model_name,
+    annual_price: Faker::Commerce.price,
+    installments: rand(0..12),
+    user: user
+  )
+end
