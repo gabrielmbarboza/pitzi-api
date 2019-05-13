56709cad51ef9c055c1d252b0220e8220949fa15
Gabriel Barboza <gabrielmbarboza@gmail.com>
2019-05-12 22:39:15 -0300

    Adds CORS support

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,12 @@ module PitziApi
     config.api_only = true
 
     config.autoload_paths += %W(#{config.root}/lib)
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
   end
 end
